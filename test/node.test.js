c5645fc6c73faac44a8549030a60809ff52b1895
bocheng.zbc <bocheng.zbc@alibaba-inc.com>
2016-06-13 16:54:16 +0800

    init ava

--- test/node.test.js
+++ test/node.test.js
@@ -1,17 +1,11 @@
-/**
- * 这是一个单元测试的栗子
- *
- * 执行单元测试请安装 @ali/def-ci 插件, 运行 def ci test
- */
+import test from 'ava';
 
-// chai 语法：http://chaijs.com/api/
+test('foo', t => {
+    t.pass();
+});
 
-var expect = require('chai').expect;
+test('bar', async t => {
+    const bar = Promise.resolve('bar');
 
-describe('just a test example', function() {
-
-  it('#这是一个例子', function() {
-    expect(1).to.equal(1);
-  });
-
+    t.is(await bar, 'bar');
 });
